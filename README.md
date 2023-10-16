314b78984aebfc193a6e5fa5dc7162a9579ed97a
ryanthewise <ryan.black1218@gmail.com>
2023-10-15 22:52:21 -0500

    buggy base code, and initial README.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# OrionSoft
+# OrionSoft Phonics Trainer
+## Ryan Black    Last Updated: 15 OCT 2023
+
+## Description
+The **OrionSoft Phonics Trainer** is a GUI application designed to help users (toddlers) improve their pronunciation of English letters. The user is prompted to pronounce a series of letters, which the application then attempts to recognize using Google's Web Speech API. If the pronunciation is correct, the user advances to the next letter. If incorrect, the user receives feedback on what the system recognized.
+
+## Features
+- Real-time volume visualization
+- Integration with Google Web Speech API for speech recognition
+- Feedback system to inform users about the correctness of their pronunciation
+- Simple and intuitive GUI
+
+## Requirements
+- Python 3.x
+- Libraries:
+  - tkinter
+  - numpy
+  - pyaudio
+  - pydub (with ffmpeg)
+  - speech_recognition
+
+```bash
+pip install numpy pyaudio pydub SpeechRecognition
+```
+
+## Setup & Installation
+- Clone the repository
+- Install the required libraries
+- Run the application
+
+## Usage
+- Run the application
+- ️Click "Start Listening".
+- Pronounce the letter that appears on the screen.
+- If the system recognizes the letter, you will advance to the next letter.
+
+## Known Issues
+- The application might encounter issues with audio recognition in noisy environments or with unclear pronunciations.
+- Ensure you have a stable internet connection since the Google Web Speech API requires it.
+
+## Contributing
+If you'd like to contribute to this project, please submit a pull request with your changes.
+
+## License
+This project is open-source. Feel free to use, modify, and distribute as you see fit.
---
+++ main.py
@@ -0,0 +1,110 @@
+import io
+import tkinter as tk
+from tkinter import messagebox
+import numpy as np
+import pyaudio
+from pydub import AudioSegment
+import speech_recognition as sr
+
+# Initialize required variables
+letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+current_letter_index = 0
+CHUNK_SIZE = 1024
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNK_SIZE)
+
+# Set up the GUI window
+window = tk.Tk()
+window.title("Letter Pronunciation Trainer")
+
+canvas = tk.Canvas(window, width=300, height=300)
+canvas.pack(pady=100)
+
+# Label to display the current letter
+letter_label = tk.Label(window, font=('Arial', 100))
+
+# Center the label on the canvas
+canvas.create_window(150, 150, window=letter_label)
+
+def get_volume():
+    # Read a chunk of audio and compute RMS
+    data = np.frombuffer(stream.read(CHUNK_SIZE, exception_on_overflow=False), dtype=np.int16)
+    rms = np.sqrt(np.maximum(np.mean(data**2), 0))
+    print(rms)
+    scale_factor = 300  # You can adjust this value to make the effect more or less pronounced
+    circle_radius = 100 + (rms / 3000) * scale_factor
+
+    # Ensure circle_radius is finite and valid
+    if np.isfinite(circle_radius):
+        canvas.coords(circle, 150-circle_radius, 150-circle_radius, 150+circle_radius, 150+circle_radius)
+
+    # Continue updating the circle
+    window.after(50, get_volume)
+
+
+def record_and_recognize():
+    global current_letter_index
+
+    # Recording audio
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source, timeout=5, phrase_time_limit=5)  # Listen for a maximum of 5 seconds
+
+    # Combine prerecorded audio with the recorded audio
+    test_audio = AudioSegment.from_wav(r"C:\Users\ryanb\Documents\Code\OrionSoft\TEST.wav")
+    user_audio = AudioSegment.from_wav(io.BytesIO(audio.get_wav_data()))
+    combined_audio = test_audio + user_audio
+
+    # Convert combined audio to WAV format in a BytesIO object
+    combined_audio_buffer = io.BytesIO()
+    combined_audio.export(combined_audio_buffer, format="wav")
+    combined_audio.export("combined_audio_test.wav", format="wav")
+    combined_audio_buffer.seek(0)
+
+    # Convert the WAV data to AudioData for the speech recognition library
+    audio_data_for_recognition = sr.AudioData(combined_audio_buffer.getvalue(), 44100, 2)
+
+    # Recognizing the spoken letter
+    try:
+        recognized_text = r.recognize_google(audio_data=audio_data_for_recognition).upper().strip()
+        print(f"Recognized text: {recognized_text}")
+
+        # Strip the "TEST" part
+        recognized_text = recognized_text.replace("TEST", "").strip()
+        print(f"Stripped text: {recognized_text}")
+
+        # Check if recognized text matches the current letter or its phonetic sound
+        acceptable_responses = {
+            "A": ["A", "AY"],
+            "B": ["B", "BE", "BEE"],
+            # You can add more phonetic variations for other letters as needed.
+        }
+
+        if recognized_text in acceptable_responses.get(letters[current_letter_index], [letters[current_letter_index]]):
+            current_letter_index += 1
+            if current_letter_index >= len(letters):
+                messagebox.showinfo("Success", "All letters recognized!")
+                window.quit()
+            else:
+                letter_label.config(text=letters[current_letter_index])
+        else:
+            messagebox.showwarning("Try Again", f"Expected {letters[current_letter_index]}, but heard {recognized_text}.")
+
+    except sr.UnknownValueError as e:
+        print(e)  # This will print the exception details to the terminal
+        messagebox.showerror("Error", "Could not recognize the audio!")
+
+# Display the first letter
+letter_label.config(text=letters[current_letter_index])
+
+# Create a circle on the canvas
+circle = canvas.create_oval(50, 50, 250, 250, outline='blue', width=2)
+
+# Button to start the recording and recognition process
+start_button = tk.Button(window, text="Start Listening", command=record_and_recognize)
+start_button.pack(pady=20)
+
+# Start volume meter
+get_volume()
+
+window.mainloop()
